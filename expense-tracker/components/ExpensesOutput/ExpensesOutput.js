fe80cfbaf74e91476ed952770284a8c2eaa87307
muhammedeid <muhammed.eid@bld.ai>
2022-09-26 01:46:50 +0200

    expense tracker

---
+++ expense-tracker/components/ExpensesOutput/ExpensesOutput.js
@@ -0,0 +1,38 @@
+import { StyleSheet, Text, View } from "react-native";
+
+import ExpensesSummary from "./ExpensesSummary";
+import ExpensesList from "./ExpensesList";
+import { GlobalStyles } from "../../constants/styles";
+
+export default function ExpensesOutput({ expenses, expensesPeriod, fallbackText }) {
+  let content = <Text style={styles.infoText}>{fallbackText}</Text>;
+
+  if (expenses.length) {
+    content = <ExpensesList expenses={expenses} />;
+  }
+
+  return (
+    <View style={styles.container}>
+      <ExpensesSummary
+        expenses={expenses}
+        expensesPeriod={expensesPeriod}
+      />
+      {content}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    paddingBottom: 0,
+    backgroundColor: GlobalStyles.colors.primary700
+  },
+  infoText: {
+    color: '#FFFFFF',
+    fontSize: 16,
+    textAlign: 'center',
+    marginTop: 32
+  }
+});
